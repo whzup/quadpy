b98698cfe8db99db927e77728b232ca429729412
whzup <39431903+whzup@users.noreply.github.com>
2020-02-10 17:49:21 +0100

    Add file for chen_krizek_liu pyramid scheme

---
+++ quadpy/pyramid/_chen_krizek_liu.py
@@ -0,0 +1,20 @@
+import numpy
+from sympy import Rational as frac
+from sympy import sqrt
+
+from ..helpers import article, untangle
+from ._helpers import PyramidScheme
+
+citation = article
+
+citation = article(
+    authors=["Chuan Mia Chen", "Michal Křížek", "Liping Liu"]
+    title="Numerical Integration over Pyramids",
+    journal="Advances in Applied Mathematics and Mechanics",
+    volume="5",
+    number="3",
+    year="2013",
+    pages="309-320",
+    url="https://doi.org/10.1017/S2070073300002691",
+)
+
